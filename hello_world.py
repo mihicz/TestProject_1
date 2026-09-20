a9fbcbe70c6655d9bfa9fd42eb25da1b09d89103
mihicz <mihaljakub@gmail.com>
2026-09-20 21:54:38 +0200

    Add current date and time output

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from datetime import datetime
+
+
+def main():
+    print("Hello, World!")
+    print("Current date and time:", datetime.now())
+
+
+if __name__ == "__main__":
+    main()
